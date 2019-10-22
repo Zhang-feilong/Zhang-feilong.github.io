bf3687da05e5465c44466694439d10d149cb14ac
Zhang-feilong <Zhang.feilong1@foxmail.com>
2019-10-23 02:19:10 +0800

    Site updated: 2019-10-23 02:19:09

--- photos/ins.js
+++ photos/ins.js
@@ -116,11 +116,11 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://raw.githubusercontent.com/Zhang-feilong/pictureofblog/master/min_picture/' + data.link[i] + '.min.jpg';
-          var src = 'https://raw.githubusercontent.com/Zhang-feilong/pictureofblog/master/pictures/' + data.link[i];
+          var minSrc = 'https://raw.githubusercontent.com/Zhang-feilong/pictureofblog/master/min_photos/' + data.link[i] + '.min.jpg';
+          var src = 'https://raw.githubusercontent.com/Zhang-feilong/pictureofblog/master/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
-          
+          src += '.jpg';
 
           liTmpl += '<figure class="thumb" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject">\
                 <a href="' + src + '" itemprop="contentUrl" data-size="640x640" data-type="' + type + '" data-target="' + target + '">\
